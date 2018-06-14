334d2dd03acb274dae532e489daaa0a3f778159d
Dmytro Hrishko <dimagrshk@gmail.com>
2018-06-14 14:22:04 +0300

    added arg for start with custom url

--- esparse/main.py
+++ esparse/main.py
@@ -1,7 +1,18 @@
 from time import sleep
+import argparse
+
 
 from parse import getLinks, parseRecipes
 from estack import connect_elasticsearch, create_index, store_record
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument(
+    "-u", "--url",
+    required=False,
+    default='https://www.allrecipes.com/recipes/96/salad/',
+    help="url for parsing")
+args = vars(ap.parse_args())
 
 def getNstore():
     es = connect_elasticsearch()
@@ -11,7 +22,7 @@ def getNstore():
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
         'Pragma': 'no-cache'
     }
-    url = 'https://www.allrecipes.com/recipes/96/salad/'
+    url = args['url']
     links = getLinks(url=url, headers=headers)
 
     for link in links:
